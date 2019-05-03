aa6f394befcdb06fb01f6290abea006c57b08bb6
Oscar Jr <oscarrsj@gmail.com>
2019-05-03 19:46:00 -0300

    REmovendo arquivos

--- src/main/java/br/desafio/pitang/resource/UsuarioResource.java
+++ src/main/java/br/desafio/pitang/resource/UsuarioResource.java
@@ -6,7 +6,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
